b517649c429e66b8eae62a44785d76bfa52000a3
Ryu <zoelabbb@gmail.com>
2025-01-06 22:37:58 +0700

    Remove unused update method from HeroController and add a comment for getHeroData

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -7,40 +7,9 @@ use Illuminate\Http\Request;
 
 class HeroController extends Controller
 {
+    // Ambil data hero
     public function getHeroData()
     {
         return response()->json(Hero::first());
-    }
-
-
-    public function update(Request $request, $id)
-    {
-        $hero = Hero::find($id);
-
-        if (!$hero) {
-            return response()->json(['message' => 'Hero not found'], 404);
-        }
-
-        // Validasi input
-        $request->validate([
-            'title' => 'required|string',
-            'subTitle' => 'required|string',
-            'primaryBtn' => 'required|string',
-            'primaryBtnURL' => 'required|url',
-            'src' => 'required|url',
-            'alt' => 'required|string',
-        ]);
-
-        // Update data Hero
-        $hero->update([
-            'title' => $request->input('title'),
-            'subTitle' => $request->input('subTitle'),
-            'primaryBtn' => $request->input('primaryBtn'),
-            'primaryBtnURL' => $request->input('primaryBtnURL'),
-            'src' => $request->input('src'),
-            'alt' => $request->input('alt'),
-        ]);
-
-        return response()->json($hero);
     }
 }
